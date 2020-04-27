f2fb07d7de07754e8b5bad5fbb05290519d03994
DaniloBNascimento <danilobastos2@gmail.com>
2020-04-27 16:33:59 -0300

    update in main.py

---
+++ main.py
@@ -0,0 +1,53 @@
+# Descrição: Script criado para consumir dados de uma API sobre o corona vírus.
+# Autor: Danilo Bastos Nascimento
+# Data de criação: 25/04/2020
+# ----------------------------------------------
+# Importando módulos necessários
+import json
+import requests
+print("Aguardando estabelecer conexão...")
+# Definindo variáveis
+# Validando conexão com o servidor de dados (API)
+try:
+    url = requests.get('https://api.covid19api.com/summary')
+except:
+    print("Servidor de dados indisponível ou falha na conexão!")
+    exit(0)
+content = json.loads(url.content)
+count = -1
+validate = url.status_code
+# Main execução:
+print("Estado atual da conexão: "+str(validate))
+# chave e valor JSON
+#print(content.keys())
+print(" ")
+print("Status global:")
+# variaveis de status global
+world_total_cases = content['Global']['TotalConfirmed']
+world_total_mortes = content['Global']['TotalDeaths']
+try:
+    world_percent_deaths = (world_total_mortes / world_total_cases) * 100
+except ZeroDivisionError:
+    world_percent_deaths = 0
+str_world_percent_deaths = str(world_percent_deaths)
+print("Total de casos: "+str(world_total_cases))
+print("Total de mortes: "+str(world_total_mortes))
+print("Letalidade Mundial: "+str_world_percent_deaths+"%")
+print("-------------------------------------------------")
+print("Relatorio por pais/regiao:")
+while count != 245:
+    count = count + 1
+    num_pais = content['Countries'][count]
+    num_cases = num_pais['TotalConfirmed']
+    pais = content['Countries'][count]['Country']
+    deaths = content['Countries'][count]['TotalDeaths']
+    try:
+        percent_deaths = (deaths / num_cases) * 100
+    except ZeroDivisionError:
+        percent_deaths = 0
+    print("País/Região: "+str(pais))
+    print("Número de casos total: " +str(num_pais['TotalConfirmed']))
+    print("Número de mortes: "+str(deaths))
+    str_percents_deaths = str(percent_deaths)
+    print("Letalidade: "+str_percents_deaths[0:4]+"%")
+    print("")
